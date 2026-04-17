a43b0c7302833347e9258accca8f8a17f9d9b498
jeongwoo78 <poly0533@gmail.com>
2026-04-18 04:47:11 +0900

    3

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -38,7 +38,7 @@ const googleProvider = new GoogleAuthProvider();
 const appleProvider = new OAuthProvider('apple.com');
 
 // 신규 유저 초기 크레딧 (폐쇄 테스트: $10.00, 프로덕션: $0.30)
-const INITIAL_FREE_CREDITS = 10.00;
+const INITIAL_FREE_CREDITS = 0.30;
 
 // 유저 문서 초기화 (첫 로그인 시 credits 필드 생성)
 const ensureUserDoc = async (userId, email) => {
